ee2bb2f39fd0902c1bd45bf0a5157404eb7dd982
Filip Razek <filip.razek@protonmail.com>
2023-09-04 22:05:30 +0200

    Fix minor bugs

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import { createServer } from "cors-anywhere";
 
-const originWhitelist = ["https://ping-me-ucyi.onrender.com/"];
+const originWhitelist = ["https://ping-me-ucyi.onrender.com"];
 const port = 3001;
 
 createServer({
-    originWhitelist,
-}).listen(3001, () => {
+  originWhitelist,
+}).listen(port, () => {
   console.log("Listening on port " + port);
 });
